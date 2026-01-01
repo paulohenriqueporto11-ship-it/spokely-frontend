d860139ba9df1db0398a7da82e06d551b24ae72f
paulohenriqueporto11-ship-it <paulohenriqueporto11@gmail.com>
2026-01-01 12:16:16 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,3 @@
 // js/config.js
-
-// URL do seu Backend no Render (troque pelo link real quando tiver)
-// IMPORTANTE: Não coloque a barra '/' no final
-const API_URL = "https://spokely-backend.onrender.com"; 
-
-// Seu ID fixo pro MVP
+const API_URL = "https://spokely-backend.onrender.com"; // SEM BARRA NO FINAL
 const USER_ID = "81d8e5b7-edb2-4227-9fb2-8d4863b51414";
