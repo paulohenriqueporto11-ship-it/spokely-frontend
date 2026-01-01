58f77235c5cdbcf5ca29b0032e94ab35e403b412
paulohenriqueporto11-ship-it <paulohenriqueporto11@gmail.com>
2026-01-01 12:30:23 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // js/config.js
-const API_URL = "https://spokely-backend.onrender.com"; // SEM BARRA NO FINAL
+const API_URL = "https://spokely-backend-1.onrender.com"; // SEM BARRA NO FINAL
 const USER_ID = "81d8e5b7-edb2-4227-9fb2-8d4863b51414";
